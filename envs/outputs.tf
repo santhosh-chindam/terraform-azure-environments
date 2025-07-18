0a88123afea2ba97f51c6454bf37c737f1555aab
santhosh-chindam <70210624+santhosh-chindam@users.noreply.github.com>
2025-07-18 21:20:26 +0530

    Create outputs.tf

---
+++ envs/outputs.tf
@@ -0,0 +1,15 @@
+output "vnet_name" {
+  value = module.network.vnet_name
+}
+
+output "vnet_id" {
+  value = module.network.vnet_id
+}
+
+output "subnet_ids" {
+  value = module.network.subnet_ids
+}
+
+output "nsg_id" {
+  value = module.network.nsg_id
+}
