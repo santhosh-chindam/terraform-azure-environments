aa5e7b85d4a08e24a1c64b4203e6627c39fe3841
santhosh-chindam <70210624+santhosh-chindam@users.noreply.github.com>
2025-07-18 21:37:03 +0530

    Update main.tf

--- envs/main.tf
+++ envs/main.tf
@@ -21,3 +21,67 @@ module "vnet" {
 
   depends_on = [azurerm_resource_group.rg]
 }
+
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "nic-${var.environment}-${var.location}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = module.vnet.subnet_ids[0]
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "pip-${var.environment}-${var.location}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  tags                = var.tags
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-${var.environment}-${var.location}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = lower(replace("st${var.environment}${var.location}", "-", ""))
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = var.tags
+}
+
