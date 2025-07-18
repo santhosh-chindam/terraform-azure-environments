5e2e2ec25845f577440ea11f2498f0b5df025fdd
santhosh-chindam <70210624+santhosh-chindam@users.noreply.github.com>
2025-07-18 21:53:17 +0530

    Update outputs.tf

--- envs/outputs.tf
+++ envs/outputs.tf
@@ -1,15 +1,15 @@
 output "vnet_name" {
-  value = module.network.vnet_name
+  value = module.vnet.vnet_name
 }
 
 output "vnet_id" {
-  value = module.network.vnet_id
+  value = module.vnet.vnet_id
 }
 
 output "subnet_ids" {
-  value = module.network.subnet_ids
+  value = module.vnet.subnet_ids
 }
 
 output "nsg_id" {
-  value = module.network.nsg_id
+  value = module.vnet.nsg_id
 }
